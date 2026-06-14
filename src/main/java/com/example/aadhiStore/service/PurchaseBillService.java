d0995698f78e0cce096d549b9790f69511499d94
arul <arulrajan260@gmail.com>
2026-06-14 18:42:44 +0530

    qty binding

--- src/main/java/com/example/aadhiStore/service/PurchaseBillService.java
+++ src/main/java/com/example/aadhiStore/service/PurchaseBillService.java
@@ -2,8 +2,12 @@ package com.example.aadhiStore.service;
 
 import com.example.aadhiStore.entity.CashBill;
 import com.example.aadhiStore.entity.PurchaseBill;
+import com.example.aadhiStore.entity.PurchaseItem;
+import com.example.aadhiStore.repository.ProductRepository;
 import com.example.aadhiStore.repository.PurchaseBillRepository;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,6 +16,9 @@ import java.util.List;
 public class PurchaseBillService {
 
     private final PurchaseBillRepository purchaseBillRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
 
     public PurchaseBillService(PurchaseBillRepository purchaseBillRepository) {
         this.purchaseBillRepository = purchaseBillRepository;
@@ -25,8 +32,40 @@ public class PurchaseBillService {
         return purchaseBillRepository.findById(id).orElseThrow(()->new EntityNotFoundException("Purchase Bill not found" +id));
     }
 
+    @Transactional
     public PurchaseBill createPurchaseBill(PurchaseBill purchaseBill) {
         purchaseBill.setPurchaseBillNo(null);
-        return purchaseBillRepository.save(purchaseBill);
+
+        if (purchaseBill.getItems() != null) {
+            purchaseBill.getItems().forEach(item ->
+                    item.setPurchaseBill(purchaseBill));
+        }
+
+        PurchaseBill savedBill = purchaseBillRepository.save(purchaseBill);
+        if (savedBill.getItems() != null) {
+
+            for (PurchaseItem item : savedBill.getItems()) {
+
+                productRepository.findByCode(item.getProductCode())
+                        .ifPresent(product -> {
+
+                            Double currentPacks =
+                                    product.getNoOfPacks() == null
+                                            ? 0.0
+                                            : product.getNoOfPacks();
+
+                            Double purchasedQty =
+                                    item.getQuantity() == null
+                                            ? 0.0
+                                            : item.getQuantity();
+
+                            product.setNoOfPacks(currentPacks + purchasedQty);
+
+                            productRepository.save(product);
+                        });
+            }
+        }
+
+        return savedBill;
     }
 }
